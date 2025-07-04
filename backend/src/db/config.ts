b8a9bfc9b8efe4ae6d71c2cbacf2c769fa1123af
IOpazoR <ignacio.opazo@uc.cl>
2025-07-04 17:54:06 -0400

    fix: db config

--- backend/src/db/config.ts
+++ backend/src/db/config.ts
@@ -1,10 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-    "millacero_db",
-    "millacero_user",
-    "123456", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
+  process.env.DB_NAME || "millacero_db",
+  process.env.DB_USER || "millacero_user",
+  process.env.DB_PASS || "password",
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    logging: false,
+  }
+);
